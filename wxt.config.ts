214efbbc34b3db09d7d32d6f9b5629e1e7aa9503
王兴家 <xingwangzhe@outlook.com>
2026-03-27 14:22:52 +0800

    chore: set explicit manifest version

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   manifest: {
     name: 'fakeclaw',
+    version: '0.0.0',
     host_permissions: ['https://tieba.baidu.com/*'],
     permissions: ['storage'],
   },
