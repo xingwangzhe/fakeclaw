f3c39422f92a80dc738c8d1e51c46ee83235a6c0
王兴家 <xingwangzhe@outlook.com>
2026-03-27 14:22:30 +0800

    chore: set explicit extension name in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-vue'],
   manifest: {
+    name: 'fakeclaw',
     host_permissions: ['https://tieba.baidu.com/*'],
     permissions: ['storage'],
   },
